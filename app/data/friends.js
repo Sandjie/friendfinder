10ece0c4a338678ec490d743b22e297fd9103f9c
Sandra <Sandra@Sandras-MacBook-Air.local>
2017-07-28 18:52:49 -0400

    friendfinder2

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,8 +15,40 @@ var friends = [
  ]
 },
 	{
-	name: " Shithead",
-	photo: "https://68.media.tumblr.com/8fc255ca98c8e775273d8fe1e22cff72/tumblr_o78nyjUMOi1uhn8mdo1_1280.jpg",
+	name: " Crazy Lady",
+	photo: "http://i0.kym-cdn.com/photos/images/newsfeed/000/173/576/Wat8.jpg?1315930535",
+	scores: ["1",
+	"2",
+	"2,",
+	"4",
+	"5",
+	"4",
+	"3",
+	"2",
+	"1",
+	"2"
+ ]
+},
+
+{
+	name: " Sexy Boy",
+	photo: "https://s-media-cache-ak0.pinimg.com/originals/c2/21/01/c221017f01fc4a05a52161cc9f717565.jpg",
+	scores: ["2",
+	"2",
+	"3,",
+	"5",
+	"4",
+	"5",
+	"4",
+	"3",
+	"2",
+	"2"
+ ]
+},
+
+{
+	name: " Handlebars",
+	photo: "http://lefunny.net/wp-content/uploads/2014/08/funny-people-pics.jpg",
 	scores: ["1",
 	"2",
 	"3,",
@@ -31,40 +63,8 @@ var friends = [
 },
 
 {
-	name: " Shithead",
-	photo: "https://68.media.tumblr.com/8fc255ca98c8e775273d8fe1e22cff72/tumblr_o78nyjUMOi1uhn8mdo1_1280.jpg",
-	scores: ["1",
-	"2",
-	"3,",
-	"5",
-	"4",
-	"5",
-	"4",
-	"3",
-	"2",
-	"2"
- ]
-},
-
-{
-	name: " Shithead",
-	photo: "https://68.media.tumblr.com/8fc255ca98c8e775273d8fe1e22cff72/tumblr_o78nyjUMOi1uhn8mdo1_1280.jpg",
-	scores: ["1",
-	"2",
-	"3,",
-	"5",
-	"4",
-	"5",
-	"4",
-	"3",
-	"2",
-	"2"
- ]
-},
-
-{
-	name: " Shithead",
-	photo: "https://68.media.tumblr.com/8fc255ca98c8e775273d8fe1e22cff72/tumblr_o78nyjUMOi1uhn8mdo1_1280.jpg",
+	name: " Pucker Up",
+	photo: "https://lh3.googleusercontent.com/xcuwTIagjQGn7pKXaJppg1LdTD6qheMbfFsk2T5ECSk27zOE2v5UZiKoNGaCipzjMzQ=w300",
 	scores: ["1",
 	"2",
 	"3,",
